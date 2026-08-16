63dfe0e48e16a083e904a2e52456578a681cb828
atharva23r <atharingane@gmail.com>
2026-08-16 23:11:55 +0530

    program

---
+++ loops/factors.java
@@ -0,0 +1,22 @@
+package loops;
+
+import java.util.Scanner;
+
+public class factors {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number");
+        int n=sc.nextInt();
+        sc.close();
+        for(int i=1;i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                System.out.print(i+" ");
+                if(i!=n/i){
+                    System.out.print(n/i+" ");
+                }
+            }
+        }
+
+    }
+    
+}
