f50ac16c965f78273f6cb7c3cadc9f5c48131cc2
atharva23r <atharingane@gmail.com>
2026-09-12 22:42:43 +0530

    update

--- pattern/floydtriangle.java
+++ pattern/floydtriangle.java
@@ -1,6 +1,6 @@
 package pattern;
 import java.util.Scanner;
-public class floudtriangle {
+public class floydtriangle {
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("enter the number");
---
+++ pattern/triverticallyflipped.java
@@ -0,0 +1,19 @@
+package pattern;
+import java.util.Scanner;
+public class triverticallyflipped {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the number");
+        int n=sc.nextInt();
+        sc.close();
+        for (int i=1;i<=n;i++){
+            for(int j=n-1;j>=i;j--){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
